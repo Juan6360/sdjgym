12779d54bd36c10ad7b08e4c402a02d975bfe308
Juan6360 <162759252+Juan6360@users.noreply.github.com>
2024-10-27 21:54:33 -0500

    Up To Date

---
+++ sdjgym/src/test/java/co/sdj/sdjgym/sdjgym_api/SdjgymApplicationTests.java
@@ -0,0 +1,13 @@
+package co.sdj.sdjgym.sdjgym_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
