dcf71f1d7398d9b165b871fd4b3c49d3e3882b26
57302 <salog8478@gmail.com>
2024-10-28 02:53:45 -0500

    se agrego paquetes data.dao, croscutting.exception

--- sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/adapter/dto/CityDTOAdapter.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/adapter/dto/CityDTOAdapter.java
@@ -1,5 +1,30 @@
 package co.sdj.sdjgym.businesslogic.adapter.dto;
 
-public class CityDTOAdapter {
+import co.sdj.sdjgym.businesslogic.adapter.Adapter;
+import co.sdj.sdjgym.dto.CityDTO;
+import co.sdj.sdjgym.domain.CityDomain;
+
+
+public final class CityDTOAdapter implements Adapter<CityDomain, CityDTO> {
+	
+	private static final Adapter<CityDomain, CityDTO> instance =  new CityDTOAdapter();
+	
+	private CityDTOAdapter() {
+		
+	}
+
+	@Override
+	public CityDomain adaptSource(CityDTO data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CityDTO adaptTarget(CityDomain data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
 
 }
--- sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/city/impl/UpdateCityImpl.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/city/impl/UpdateCityImpl.java
@@ -1,32 +1,35 @@
 package co.sdj.sdjgym.businesslogic.usecase.city.impl;
 
 import co.sdj.crosscutting.helpers.ObjectHelper;
+import co.sdj.sdjgym.businesslogic.adapter.entity.CityEntityAdapter;
 import co.sdj.sdjgym.businesslogic.usecase.city.UpdateCity;
+import co.sdj.crosscutting.exceptions.BusinessLogicUcoBetException;
 import co.sdj.sdjgym.data.dao.DAOFactory;
 import co.sdj.sdjgym.domain.CityDomain;
 
-public final class UpdateCityImpl implements UpdateCity{
-
+public final class UpdateCityImpl implements UpdateCity {
+	
 	private DAOFactory daoFactory;
 	
 	public UpdateCityImpl(DAOFactory daoFactory) {
-		
+		setDaoFactory(daoFactory);
 	}
-	
-	
-	
+
 	@Override
-	public void execute(CityDomain data) {
-		// TODO Auto-generated method stub
+	public void execute(final CityDomain data) {
+		
+		var cityEntity = CityEntityAdapter.getCityEntityAdapter().adaptSource(data);
+		daoFactory.getCityDAO().update(cityEntity);
 		
 	}
-	
+
 	private void setDaoFactory(final DAOFactory daoFactory) {
-		if(ObjectHelper.isNull(daoFactory)) {
-			
+		if (ObjectHelper.isNull(daoFactory)) {
+			var userMessage = "se ha presentado un problema inesperado tratando de llevar a cabo la modificación de la información de la cuidad deseada.porfavor intenta de nuevo y si el problema persiste llame a luz mery rios Alzate..";
+			var technicalMessage = "El dao factory requerido para crear la clase que se actualiza la cuidad llegó nula.";
+			throw BusinessLogicUcoBetException.crear(userMessage, technicalMessage);
 		}
-		
-		this.daoFactory= daoFactory;
+		this.daoFactory = daoFactory;
 	}
 
 }
