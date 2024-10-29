a5dd0dcffffbc09e16123a1404fbb8bb8e3b8c79
Juan6360 <162759252+Juan6360@users.noreply.github.com>
2024-10-29 00:55:26 -0500

    Coach Entity

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/entity/CoachEntity.java
@@ -0,0 +1,104 @@
+package co.sdj.sdjgym.entity;
+
+import java.util.Date;
+
+import co.sdj.crosscutting.helpers.TextHelper;
+import co.sdj.crosscutting.helpers.UUIDHelper;
+
+public class CoachEntity extends DomainEntity {
+	
+	private String firstName;
+	private String middleName;
+	private String firstSurName;
+	private String secondSurName;
+	// TODO atributo de identificacion
+	private String phoneNumber;
+	private String emergencyNumber;
+	private String email;
+	private Date birthDate;
+	// TODO atributos de departamento y municipio
+	private String address;
+	// TODO atributo de especialidad
+	
+	protected CoachEntity() {
+		super(UUIDHelper.getDefault());
+		setFirstName(TextHelper.EMPTY);
+		setMiddleName(TextHelper.EMPTY);
+		setFirstSurName(TextHelper.EMPTY);
+		setSecondSurName(TextHelper.EMPTY);
+		setPhoneNumber();
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getFirstSurName() {
+		return firstSurName;
+	}
+
+	public void setFirstSurName(String firstSurName) {
+		this.firstSurName = firstSurName;
+	}
+
+	public String getSecondSurName() {
+		return secondSurName;
+	}
+
+	public void setSecondSurName(String secondSurName) {
+		this.secondSurName = secondSurName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmergencyNumber() {
+		return emergencyNumber;
+	}
+
+	public void setEmergencyNumber(String emergencyNumber) {
+		this.emergencyNumber = emergencyNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
