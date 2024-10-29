8a2c4f6b633113a2097e6ffd23427219ae232d48
57302 <salog8478@gmail.com>
2024-10-29 14:41:57 -0500

    se agrgaron los archivos countrysqlserverdao y citysqlserverdao

--- sdjgym/src/main/java/co/sdj/sdjgym/data/dao/impl/sql/sqlserver/CitySqlServerDAO.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/data/dao/impl/sql/sqlserver/CitySqlServerDAO.java
@@ -11,15 +11,32 @@ import co.sdj.sdjgym.entity.CityEntity;
 import co.sdj.sdjgym.entity.CountryEntity;
 
 public class CitySqlServerDAO extends SqlDAO implements CityDAO {
-	
-	public CitySqlServerDAO(final Connection connection) {
+
+	protected CitySqlServerDAO(final Connection connection) {
 		super(connection);
 	}
 
 	@Override
-	public void create(CityEntity data) {
-		// TODO Auto-generated method stub
-		
+	public void create(final CityEntity data) {
+		 
+		final StringBuilder statement = new StringBuilder();
+		statement.append("INSERT INTO City(id, name, state) VALUES (?, ?,?)");
+ 
+		try (final var preparedStatement = getConnection().prepareStatement(statement.toString())) {
+ 
+			preparedStatement.setObject(1, data.getId());
+			preparedStatement.setString(2, data.getName());
+			preparedStatement.setObject(3, data.getState().getId());
+ 
+			preparedStatement.executeUpdate();
+ 
+		} catch (final SQLException exception) {
+			var userMessage = "Se ha presentado un problema tratando de llevar a cabo el registro de la información del nuevo país. Por favor intente de nuevo y si el problema persiste reporte la novedad...";
+			var technicalMessage = "Se ha presentado un problema al tratar de registrar la informaciòn del nuevo país en la base de datos SQL Server. Por favor valide el log de errores para encontrar mayores detalles del problema presentado...";
+ 
+			throw DataUcoBetException.crear(userMessage, technicalMessage, exception);
+		}
+ 
 	}
 
 	@Override
@@ -41,17 +58,50 @@ public class CitySqlServerDAO extends SqlDAO implements CityDAO {
 	}
 
 	@Override
-	public void update(CityEntity data) {
-		// TODO Auto-generated method stub
+	public void update(final CityEntity data) {
 		
+		final StringBuilder statement = new StringBuilder();
+		statement.append("UPDATE City SET name = ?, state = ? WHERE id = ?");
+ 
+		try (final var preparedStatement = getConnection().prepareStatement(statement.toString())) {
+ 
+			preparedStatement.setObject(1, data.getName());
+			preparedStatement.setObject(2, data.getState().getId());
+			preparedStatement.setObject(3, data.getId());
+ 
+			preparedStatement.executeUpdate();
+ 
+		} catch (final SQLException exception) {
+			var userMessage = "Se ha presentado un problema tratando de llevar a cabo el registro de la información del nuevo país. Por favor intente de nuevo y si el problema persiste reporte la novedad...";
+			var technicalMessage = "Se ha presentado un problema al tratar de registrar la informaciòn del nuevo país en la base de datos SQL Server. Por favor valide el log de errores para encontrar mayores detalles del problema presentado...";
+ 
+			throw DataUcoBetException.crear(userMessage, technicalMessage, exception);
+		}
+ 
 	}
+		
+
 
 	@Override
 	public void delete(UUID data) {
-		// TODO Auto-generated method stub
 		
+		final StringBuilder statement = new StringBuilder();
+		statement.append("DELETE FROM City WHERE id = ?)");
+ 
+		try (final var preparedStatement = getConnection().prepareStatement(statement.toString())) {
+ 
+			preparedStatement.setObject(1, data);
+			
+ 
+			preparedStatement.executeUpdate();
+ 
+		} catch (final SQLException exception) {
+			var userMessage = "Se ha presentado un problema tratando de llevar a cabo el registro de la información del nuevo país. Por favor intente de nuevo y si el problema persiste reporte la novedad...";
+			var technicalMessage = "Se ha presentado un problema al tratar de registrar la informaciòn del nuevo país en la base de datos SQL Server. Por favor valide el log de errores para encontrar mayores detalles del problema presentado...";
+ 
+			throw DataUcoBetException.crear(userMessage, technicalMessage, exception);
+		}
+ 
 	}
-	
-
 
 }
