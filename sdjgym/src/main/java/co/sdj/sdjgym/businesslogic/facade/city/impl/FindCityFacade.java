2421a44aaf010e8bfe940e409fef915c099d89da
ServiciosDocentes <ServiciosDocentes@DESKTOP-MPJ8PMK>
2024-10-29 20:55:44 -0500

    Find City Facade

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/facade/city/impl/FindCityFacade.java
@@ -0,0 +1,7 @@
+package co.sdj.sdjgym.businesslogic.facade.city.impl;
+
+import co.sdj.sdjgym.businesslogic.facade.FacadeWithReturn;
+
+public interface FindCityFacade extends FacadeWithReturn (CityDTO, List<CityDTO>) {
+
+}
--- sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/facade/city/impl/RegisterNewCityFacadeImpl.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/facade/city/impl/RegisterNewCityFacadeImpl.java
@@ -1,6 +1,9 @@
 package co.sdj.sdjgym.businesslogic.facade.city.impl;
 
+import co.sdj.sdjgym.businesslogic.adapter.dto.CityDTOAdapter;
 import co.sdj.sdjgym.businesslogic.facade.city.RegisterNewCityFacade;
+import co.sdj.sdjgym.businesslogic.usecase.city.impl.RegisterNewCityImpl;
+import co.sdj.sdjgym.crosscutting.exceptions.BusinessLogicSdjException;
 import co.sdj.sdjgym.crosscutting.exceptions.SdjException;
 import co.sdj.sdjgym.data.dao.DAOFactory;
 import co.sdj.sdjgym.data.dao.enums.DAOSource;
@@ -15,11 +18,23 @@ public class RegisterNewCityFacadeImpl implements RegisterNewCityFacade {
 		try {
 			factory.initTransaction();
 			
+			var registerNewCityUseCase = new RegisterNewCityImpl(factory);
+			var cityDomain = CityDTOAdapter.getCityDTOAdapter().adaptSource(data);
+			
+			registerNewCityUseCase.execute(factory);
+			
 			factory.commitTransaction();
 		} catch(final SdjException exception) {
 			factory.rollbackTransaction();
+			
+			throw exception;
 		} catch(final Exception exception) {
 			factory.rollbackTransaction();
+			
+			var userMessage = "Se ha presentado un problema tratando registrar la información de la nnueva ciudad...";
+			var technicalMessage = "Se ha presentado un problema inesperado registrando la información de la nueva ciudad. Por favor revise el log para tener más detalles...";
+			
+			throw BusinessLogicSdjException.crear(userMessage, technicalMessage);
 		} finally {
 			factory.closeConnection();
 		}
