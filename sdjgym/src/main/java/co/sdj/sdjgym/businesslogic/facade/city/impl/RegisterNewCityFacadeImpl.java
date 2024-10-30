e07acc397b32f540b72e9e39bba2edeb6849aea9
ServiciosDocentes <ServiciosDocentes@DESKTOP-MPJ8PMK>
2024-10-29 20:39:36 -0500

    Comitting Transactions

--- sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/facade/city/impl/RegisterNewCityFacadeImpl.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/facade/city/impl/RegisterNewCityFacadeImpl.java
@@ -1,6 +1,7 @@
 package co.sdj.sdjgym.businesslogic.facade.city.impl;
 
 import co.sdj.sdjgym.businesslogic.facade.city.RegisterNewCityFacade;
+import co.sdj.sdjgym.crosscutting.exceptions.SdjException;
 import co.sdj.sdjgym.data.dao.DAOFactory;
 import co.sdj.sdjgym.data.dao.enums.DAOSource;
 import co.sdj.sdjgym.dto.CityDTO;
@@ -10,6 +11,18 @@ public class RegisterNewCityFacadeImpl implements RegisterNewCityFacade {
 	@Override
 	public void execute(CityDTO data) {
 		var factory = DAOFactory.getFactory(DAOSource.SQLSERVER);
+		
+		try {
+			factory.initTransaction();
+			
+			factory.commitTransaction();
+		} catch(final SdjException exception) {
+			factory.rollbackTransaction();
+		} catch(final Exception exception) {
+			factory.rollbackTransaction();
+		} finally {
+			factory.closeConnection();
+		}
 	}
 
 }
