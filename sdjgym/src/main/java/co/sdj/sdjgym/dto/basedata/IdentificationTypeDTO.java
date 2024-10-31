b2935eb09a4588ffcb6ad101895d100ed3f95426
Juan6360 <162759252+Juan6360@users.noreply.github.com>
2024-10-31 12:23:27 -0500

    IdentificatioType DTO/Entity

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/dto/basedata/IdentificationTypeDTO.java
@@ -0,0 +1,29 @@
+package co.sdj.sdjgym.dto.basedata;
+
+import co.sdj.crosscutting.helpers.TextHelper;
+import co.sdj.crosscutting.helpers.UUIDHelper;
+import co.sdj.sdjgym.dto.DomainDTO;
+
+public class IdentificationTypeDTO extends DomainDTO {
+	
+	private String name;
+
+	private IdentificationTypeDTO() {
+		super(UUIDHelper.getDefaultAsString());
+		setName(TextHelper.EMPTY);
+	}
+	
+	public static final IdentificationTypeDTO create() {
+		return new IdentificationTypeDTO();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public IdentificationTypeDTO setName(final String name) {
+		this.name = TextHelper.applyTrim(name);
+		return this;
+	}
+	
+}
---
+++ sdjgym/src/main/java/co/sdj/sdjgym/entity/basedata/IdentifiactionTypeEntity.java
@@ -0,0 +1,36 @@
+package co.sdj.sdjgym.entity.basedata;
+
+import java.util.UUID;
+
+import co.sdj.crosscutting.helpers.TextHelper;
+import co.sdj.crosscutting.helpers.UUIDHelper;
+import co.sdj.sdjgym.entity.DomainEntity;
+
+public class IdentifiactionTypeEntity extends DomainEntity {
+
+	private String name;
+	
+	protected IdentifiactionTypeEntity(UUID id) {
+		super(UUIDHelper.getDefault());
+		setName(TextHelper.EMPTY);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = TextHelper.applyTrim(name);
+	}
+	
+	@Override
+	public void setId(final UUID id) {
+		super.setId(id);
+	}
+	
+	@Override
+	public UUID getId() {
+		return super.getId();
+	}
+
+}
