8c2cabf6bcb97b028964baa69f6256cdfa4d6e22
57302 <salog8478@gmail.com>
2024-10-31 14:59:50 -0500

    create the package branch

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/domain/branch/BranchDomain.java
@@ -0,0 +1,58 @@
+package co.sdj.sdjgym.domain.branch;
+
+import java.util.UUID;
+
+import co.sdj.crosscutting.helpers.ObjectHelper;
+import co.sdj.crosscutting.helpers.TextHelper;
+import co.sdj.crosscutting.helpers.UUIDHelper;
+import co.sdj.sdjgym.domain.Domain;
+import co.sdj.sdjgym.domain.basedata.CityDomain;
+
+
+public class BranchDomain extends Domain {
+	
+	private String address;
+	private CityDomain city;
+	
+	private BranchDomain(final UUID id,final String address, final CityDomain city) {
+		super(id);
+		setAddress (address);
+		setCity(city);
+		
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public static final BranchDomain create(final UUID id, final String address, final CityDomain city) {
+		return new BranchDomain(id, address, city);
+	}
+	
+	
+	public static final BranchDomain create() {
+		return new BranchDomain(UUIDHelper.getDefault(), TextHelper.EMPTY, CityDomain.create());
+	}
+
+	private void setAddress(final String address) {
+		this.address = TextHelper.applyTrim(address);
+	}
+
+	@Override
+	public UUID getId() {
+		return super.getId();
+	}
+
+	public CityDomain getState() {
+		return city;
+	}
+	
+
+	private void setCity(CityDomain city) {
+		this.city = ObjectHelper.getDefault(city, CityDomain.create());
+		
+		
+	}
+	
+
+}
