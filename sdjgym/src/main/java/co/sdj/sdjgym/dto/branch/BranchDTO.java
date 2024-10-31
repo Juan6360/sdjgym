69a604dff000cfe8b1bdc041f66092c5d16b4d16
57302 <salog8478@gmail.com>
2024-10-31 15:52:27 -0500

    create the package branch DTO

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/dto/branch/BranchDTO.java
@@ -0,0 +1,56 @@
+package co.sdj.sdjgym.dto.branch;
+
+import co.sdj.crosscutting.helpers.ObjectHelper;
+import co.sdj.crosscutting.helpers.TextHelper;
+import co.sdj.crosscutting.helpers.UUIDHelper;
+import co.sdj.sdjgym.dto.DomainDTO;
+import co.sdj.sdjgym.dto.basedata.CityDTO;
+
+public class BranchDTO extends DomainDTO {
+	
+	private String address;
+	private CityDTO city;
+	
+
+	public BranchDTO() {
+		super(UUIDHelper.getDefaultAsString());
+		setAddress(TextHelper.EMPTY);
+		setCity(CityDTO.create());
+	}
+
+	public static final BranchDTO create() {
+		return new BranchDTO();
+		
+	}
+	public String getAddress() {
+		return address;
+	}
+	
+	public BranchDTO setAddress(final String address) {
+		this.address = TextHelper.applyTrim (address);
+		return this;
+		
+	}
+	
+	
+	public BranchDTO setId(final String id) {
+		super.setIdentifier(id);
+		return this;
+	}
+	
+	@Override
+	public String getId() {
+		return super.getId();
+	}
+
+	public CityDTO getCity() {
+		return city;
+	}
+
+	public BranchDTO setCity(final CityDTO city) {
+		this.city = ObjectHelper.getDefault(city, CityDTO.create());
+		return this;
+	}
+}
+
+	
