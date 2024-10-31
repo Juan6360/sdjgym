05521b83442331e84b6e94830fcc9a44ccaa5822
Juan6360 <162759252+Juan6360@users.noreply.github.com>
2024-10-31 15:01:06 -0500

    IDK

--- sdjgym/src/main/java/co/sdj/sdjgym/domain/coach/CoachDomain.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/domain/coach/CoachDomain.java
@@ -4,50 +4,22 @@ import java.util.Date;
 import java.util.UUID;
 
 import co.sdj.sdjgym.domain.PersonDomain;
+import co.sdj.sdjgym.domain.basedata.CityDomain;
 import co.sdj.sdjgym.domain.basedata.EpsDomain;
 import co.sdj.sdjgym.domain.basedata.IdentificationTypeDomain;
+import co.sdj.sdjgym.domain.basedata.SpecialtyDomain;
+import co.sdj.sdjgym.domain.basedata.StateDomain;
 
-public class CoachDomain extends PersonDomain {
+public class CoachDomain extends Domain, PersonDomain {
+	
+	private SpecialtyDomain specialty;
 
-	protected CoachDomain(UUID id, 
-							String firstName,
-							String middleName,
-							String firstSurName,
-							String secondSurName,
-							int phoneNumber,
-							String emergencyNumber,
-							String email,
-							Date birthDate,
-							UUID identificadorTipoIdentificacion,
-							IdentificationTypeDomain tipoIdentificacion,
-							int identificacion,
-							EpsDomain eps,
-							String address,
-							UUID identificatorState,
-							String state,
-							UUID identificadorCity,
-							String city) {
-		
-		super(id,
-				firstName,
-				middleName,
-				firstSurName,
-				secondSurName,
-				phoneNumber,
-				emergencyNumber,
-				email,
-				birthDate,
-				identificadorTipoIdentificacion,
-				tipoIdentificacion,
-				identificacion,
-				eps,
-				address,
-				identificatorState,
-				state,
-				identificadorCity,
-				city);
+	protected CoachDomain(UUID id, String firstName, String middleName, String firstSurName, String secondSurName,
+			String phoneNumber2, String emergencyNumber2, String email, Date birthDate,
+			IdentificationTypeDomain tipoIdentificacion, String identificacion, EpsDomain eps, String address,
+			StateDomain state2, CityDomain city2) {
+		super(id, firstName, middleName, firstSurName, secondSurName, phoneNumber2, emergencyNumber2, email, birthDate,
+				tipoIdentificacion, identificacion, eps, address, state2, city2);
 	}
-	
-	
 	
 }
