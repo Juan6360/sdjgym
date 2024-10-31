1fabd67178c3d4a7124fc2937b9b827a770117e6
57302 <salog8478@gmail.com>
2024-10-31 15:32:49 -0500

    create the package branch entity

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/entity/branch/BranchEntity.java
@@ -0,0 +1,53 @@
+package co.sdj.sdjgym.entity.branch;
+
+import java.util.UUID;
+
+import co.sdj.crosscutting.helpers.ObjectHelper;
+import co.sdj.crosscutting.helpers.TextHelper;
+import co.sdj.crosscutting.helpers.UUIDHelper;
+import co.sdj.sdjgym.entity.DomainEntity;
+import co.sdj.sdjgym.entity.basedata.CityEntity;
+
+public class BranchEntity extends DomainEntity {
+
+	
+	private String address;
+	private CityEntity city;
+	
+	
+	public BranchEntity () {
+		super(UUIDHelper.getDefault());
+		setAddress(TextHelper.EMPTY);
+		setCity(new CityEntity());
+    }
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(final String address) {
+		this.address = TextHelper.applyTrim (address);
+		
+	}
+	
+	@Override
+	public void setId(final UUID id) {
+		super.setId(id);
+		
+	}
+	
+	@Override
+	public UUID getId() {
+		return super.getId();
+	}
+
+	public CityEntity getState() {
+		return city;
+	}
+
+	public void setCity(final CityEntity state) {
+		this.city = ObjectHelper.getDefault(state, new 	CityEntity());
+	}
+
+
+	
+}
