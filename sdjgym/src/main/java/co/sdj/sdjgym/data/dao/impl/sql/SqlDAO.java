2315f8eb73faebb005d1facf4becbe252513bd5f
57302 <salog8478@gmail.com>
2024-11-07 21:02:35 -0500

    modifiqye city dao y sql dao

--- sdjgym/src/main/java/co/sdj/sdjgym/data/dao/impl/sql/SqlDAO.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/data/dao/impl/sql/SqlDAO.java
@@ -3,7 +3,6 @@ package co.sdj.sdjgym.data.dao.impl.sql;
 import java.sql.Connection;
 
 import co.sdj.crosscutting.helpers.SqlConnectionHelper;
-import co.sdj.sdjgym.crosscutting.exceptions.DataSdjException;
 
 public class SqlDAO {
 	
@@ -18,15 +17,9 @@ public class SqlDAO {
 	}
 	
 	private void setConnection(final Connection connection) {
-		validateIfConnetionIsOpen(connection);
+		SqlConnectionHelper.validateIfConnectionIsClosed(connection);
 		this.connection = connection;
 	}
 	
-	private void validateIfConnetionIsOpen(final Connection connection) {
-		if(!SqlConnectionHelper.connectionIsOpen(connection)) {
-			var userMenssage = "Se a presentado un problema inesperado tratando de llevar a cabo la operación deseada...";
-			var technicalMessage="No es posible crear un acceso a datos de tipo SQL con una conexión nula o cerrada...";
-			throw DataSdjException.crear(userMenssage, technicalMessage);
-		}
-	}
 }
+
