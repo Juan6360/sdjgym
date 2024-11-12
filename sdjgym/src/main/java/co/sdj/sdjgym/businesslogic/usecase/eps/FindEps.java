b70b41a47c23121aadf08fc01be59f16a9403419
eltuttis <jrodriguezgiraldo8@gmail.com>
2024-11-11 19:55:21 -0500

    FindEpsImpl finalizater

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/eps/FindEps.java
@@ -0,0 +1,10 @@
+package co.sdj.sdjgym.businesslogic.usecase.eps;
+
+import java.util.List;
+
+import co.sdj.sdjgym.businesslogic.usecase.UseWithReturn;
+import co.sdj.sdjgym.domain.EpsDomain;
+
+public interface FindEps extends UseWithReturn<EpsDomain, List<EpsDomain>> {
+
+}
