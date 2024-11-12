033523f4a047a04ed6a9def1ab978970adfe4ae1
eltuttis <jrodriguezgiraldo8@gmail.com>
2024-11-12 13:35:16 -0500

    UserEmailDoesNotExistsForOteruser

---
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/user/rules/UserEmailDoesNotExistsForOtherUser.java
@@ -0,0 +1,8 @@
+package co.sdj.sdjgym.businesslogic.usecase.user.rules;
+
+import co.sdj.sdjgym.businesslogic.usecase.RuleWithFactory;
+import co.sdj.sdjgym.domain.UserDomain;
+
+public interface UserEmailDoesNotExistsForOtherUser extends RuleWithFactory<UserDomain>{
+
+}
---
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/user/rules/impl/UserEmailDoesNotExistsForOtherUserImpl.java
@@ -0,0 +1,26 @@
+package co.sdj.sdjgym.businesslogic.usecase.user.rules.impl;
+
+import co.sdj.sdjgym.businesslogic.usecase.user.rules.UserEmailDoesNotExistsForOtherUser;
+import co.sdj.sdjgym.crosscutting.exceptions.BusinessLogicSdjException;
+import co.sdj.sdjgym.data.dao.DAOFactory;
+import co.sdj.sdjgym.domain.UserDomain;
+import co.sdj.sdjgym.entity.UserEntity;
+
+public class UserEmailDoesNotExistsForOtherUserImpl implements UserEmailDoesNotExistsForOtherUser{
+
+	@Override
+	public void execute(UserDomain data, DAOFactory factory) {
+		final var user = new UserEntity();
+		user.setEmail(data.getEmail());
+		
+		
+		var results = factory.getUserDAO().findByFilter(user);
+		
+		
+		if (!results.isEmpty()) {
+			var userMessage = "Ya existe un Usuario "+results.get(0).getFirstName()+" "+results.get(0).getMiddleName() +"con el mismo " + results.get(0).getEmail();
+			throw BusinessLogicSdjException.crear(userMessage);
+		}
+	}
+
+}
