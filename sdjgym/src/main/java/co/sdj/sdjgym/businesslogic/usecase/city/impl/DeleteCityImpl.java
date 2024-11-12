a09d1ad0a1dd17e1af10845781b96088925485e0
57302 <salog8478@gmail.com>
2024-11-11 19:43:01 -0500

    nn

--- sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/city/impl/DeleteCityImpl.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/city/impl/DeleteCityImpl.java
@@ -3,13 +3,16 @@ package co.sdj.sdjgym.businesslogic.usecase.city.impl;
 import java.util.UUID;
 
 import co.sdj.sdjgym.businesslogic.usecase.city.DeleteCity;
+import co.sdj.sdjgym.data.dao.DAOFactory;
 
 public final class DeleteCityImpl implements DeleteCity {
 
+	   private DAOFactory daoFactory;
+
 	@Override
-	public void execute(final UUID data) {
+	public void execute(UUID data) {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
+
--- sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/city/impl/FindCityImpl.java
+++ sdjgym/src/main/java/co/sdj/sdjgym/businesslogic/usecase/city/impl/FindCityImpl.java
@@ -7,14 +7,21 @@ import co.sdj.sdjgym.data.dao.DAOFactory;
 import co.sdj.sdjgym.domain.CityDomain;
 
 public final class FindCityImpl implements FindCity {
+
+	private DAOFactory daoFactory;
+	
 	public FindCityImpl(final DAOFactory daoFactory) {
+		setDaoFactory(daoFactory);
+	}
+
+	private void setDaoFactory(final DAOFactory daoFactory) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<CityDomain> execute(final CityDomain data) {
+	public List<CityDomain> execute(CityDomain data) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
